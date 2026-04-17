bfc6f0a92d79c8d9c8f6ea0f0cbf2228f9929646
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-04-17 11:21:01 +0900

    chore: install gg.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
+import ggPlugins from '@leftium/gg/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
+	plugins: [sveltekit(), devtoolsJson(), ...ggPlugins({ fileSink: true })]
 });
